cc17c005182311aacbbf28c4e37d83298d14d1a2
patarapolw <patarapolw@gmail.com>
2019-04-22 18:34:47 +0700

    Some fixes

--- src/web/Front.ts
+++ src/web/Front.ts
@@ -6,9 +6,9 @@ import dbEditorState from "./DbEditor/shared";
 export default class Front extends Vue {
     public render(m: CreateElement) {
         return m("div", {
-            class: ["mt-3", "container", "ml-3", "mr-3", "nav-fixed-content"]
+            class: ["mt-3", "container", "nav-fixed-content"]
         }, [
-            m("div", {class: ["row"]}, "Login to create your interactive quiz."),
+            m("div", {class: ["row", "ml-3", "mr-3"]}, "Login to create your interactive quiz."),
             m("img", {
                 class: ["mt-3", "row", "mx-auto"],
                 domProps: {src: "/screenshots/quiz1.png"}
