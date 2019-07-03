1e8a7d179e221cc4326524f2db4f43cc245e9dd3
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2019-07-03 13:21:42 +0700

    use autossh buildpack

--- .profile.d/ssh-setup.sh
+++ .profile.d/ssh-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# See https://github.com/kollegorna/heroku-buildpack-autossh
 echo $0: creating public and private key files
 
 # Create the .ssh directory
@@ -6,23 +7,15 @@ mkdir -p ${HOME}/.ssh
 chmod 700 ${HOME}/.ssh
 
 # Create the public and private key files from the environment variables.
-echo "${HEROKU_PUBLIC_KEY}" > ${HOME}/.ssh/heroku_id_rsa.pub
-chmod 644 ${HOME}/.ssh/heroku_id_rsa.pub
+echo "${HEROKU_PUBLIC_KEY}" > ${HOME}/.ssh/id_rsa.pub
+chmod 644 ${HOME}/.ssh/id_rsa.pub
 
 # Note use of double quotes, required to preserve newlines
-echo "${HEROKU_PRIVATE_KEY}" > ${HOME}/.ssh/heroku_id_rsa
-chmod 600 ${HOME}/.ssh/heroku_id_rsa
+echo "${HEROKU_PRIVATE_KEY}" > ${HOME}/.ssh/id_rsa
+chmod 600 ${HOME}/.ssh/id_rsa
 
-# Preload the known_hosts file  (see "version 2" below)
-echo '|1|sBi/rtSp2vStSlpM1LLeEKSdudg=|udgBlU5tuePGLTFHCeB+f6q1FhM= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHLMoBUMvpdE+rYgLnmQ3t2XYBLEdLGO/jwHq6g9QXoRiM4bUSL/qpmyygUT7GVLh5rEWqTFmZLRhwhWDvvdTZ8=' > ${HOME}/.ssh/known_hosts
+# Auto add the host to known_hosts
+ssh-keyscan ${REMOTE_MONGO_HOST} >> ${HOME}/.ssh/known_hosts
 
 # Start the SSH tunnel if not already running
-SSH_CMD="ssh -f -i ${HOME}/.ssh/heroku_id_rsa -N -L 4321:localhost:27017 ${REMOTE_USER}@${REMOTE_MONGO_HOST}"
-
-PID=`pgrep -f "${SSH_CMD}"`
-if [ $PID ] ; then
-    echo $0: tunnel already running on ${PID}
-else
-    echo $0 launching tunnel
-    $SSH_CMD
-fi
+autossh -M 33306 -f -N -o "ServerAliveInterval 10" -o "ServerAliveCountMax 3"  -L 4321:localhost:27017 ${REMOTE_USER}@${REMOTE_MONGO_HOST}
